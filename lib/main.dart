08d3aa6d825c0ff09d3e0796b202a6005db43b49
Tan Tran <2188362+tant@users.noreply.github.com>
2024-01-28 14:45:09 +0700

    Add usbserial library and try to ready bytes from devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:usb_serial/usb_serial.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +12,7 @@ void main() {
             backgroundColor: Colors.blue,
             title: const Text("Đây là app bar"),
           ),
-            body: const Center(child: MyWidget(true)),
+            body: const Center(child: MyWidget(false)),
         ),
       ),
       debugShowCheckedModeBanner: false,
@@ -31,7 +34,40 @@ class MyWidget extends StatefulWidget {
 class MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return widget.loading ? const CircularProgressIndicator() : const Text("NASDAQ");
-  }
+    return ElevatedButton(
+      onPressed: () async {
+        List<UsbDevice> devices = await UsbSerial.listDevices();
+        print(devices);
+        UsbPort? port;
+        if (devices.isEmpty) {
+          return;
+        } else {
+          port = await devices[0].create();
+          bool? openResult = await port?.open();
+          if ( !openResult! ) {
+            print("Failed to open");
+            return;
+          } else {
+            await port?.setDTR(true);
+            await port?.setRTS(true);
 
+            port?.setPortParameters(115200, UsbPort.DATABITS_8,
+                UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+            // print first result and close port.
+            port?.inputStream?.listen((Uint8List event) {
+              print(event);
+              port?.close();
+            });
+          }
+        }
+
+      },
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.pink,
+        backgroundColor: Colors.green,
+      ),
+      child: const Text("Press me", style: TextStyle(fontSize: 28)),
+    );
+  }
 }
